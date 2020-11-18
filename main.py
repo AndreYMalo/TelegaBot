82305343ebd92a380d0bb42d76617c90f8b0e512
AndreYMalo <72851945+AndreYMalo@users.noreply.github.com>
2020-11-18 10:34:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
+import COVID19Py
 import telebot
-bot = telebot.TeleBot("905266572:AAF0hAMoP01I2Vx-diR7_mgyhFw0NT4XWHI")
-# bot.send_message(1149221538, "Hello")
 
-# upd = bot.get_updates()
-# last_upd = upd[-1]
-# message_from_user = last_upd.message
-# print(message_from_user)
+covid19 = COVID19Py.COVID19()
+bot = telebot.TeleBot('1453562215:AAEzET3P8qhTpWM9E7Zz-cFFq-SAKBzCXUs')
 
-#print(bot.get_me())
-#def log(message, answer):
-   # print('\n ------')
-   # from datetime import datetime
-    #print(datetime.now())
-    #print("Сообщение от {0} {1}. (id = {2}) \n Текст - {3}".format(message.from_user.first_name,
-                                                                   #message.from_user.last_name,
-                                                                   #str(message.from_user.id),
-                                                                   #message.text))
+@bot.message_handler(commands=['start'])
+def start(message):
+    send_mess = f"<b>Привет {message.from_user.first_name}!</b>\nВведите страну"
+    bot.send_message(message.chat.id, send_mess, parse_mode='html')
 
-#print(answear)
+@bot.message_handler(content_types=['text'])
+def mess(message):
+    final_message = ""
+    get_message_bot = message.text.strip().lower()
+    if get_message_bot == "сша":
+        location = covid19
 
-@bot.message_handler(commands=["start"])
-def handler_start(message):
-     user_markup = telebot.types.ReplyKeyboardMarkup()
-     user_markup.row("/start","/stop")
-     user_markup.row("fsdfag", "photo")
-     user_markup.row("music", "video")
-     bot.send_message(message.from_user.id, "Выбирай!",reply_markup=user_markup)
-#
-#
-#
-#
-# #    print("ПРИШЛО СООБЩЕНИЕ ОМАГАД")
-bot.polling(none_stop=True, interval=0)
+bot.polling(none_stop=True)
